a0aceda21cd539df700060b7856f481359979269
Rubin Simons <rubin@raaf.tech>
2022-12-18 22:00:28 +0100

    Added example C interop calls.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
 // TODO: Get started with Reveal, son of Session.
+import scalanative.unsafe._
+
+@extern
+private object other {
+  def add3(in: CLongLong): CLongLong = extern
+}
+
+@extern
+private object libc {
+  def system(command: CString): CInt = extern
+}
+
+import other._
+import libc._
+
 object Main {
-  def main(args: Array[String]): Unit =
-    println("Hello, world!")
+  def main(args: Array[String]): Unit = {
+    val exit = system(c"ls -l > test.txt")
+    println(s"Hello, native Scala world, I got ${add3(3)} from a native C function.. and exit from some command was: $exit")
+  }
 }
